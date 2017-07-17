8cfdc88db408e2f2a623a1327f808bb20be0073c
bushan nagendra <bushan1891@gmail.com>
2017-07-16 18:19:00 -0700

    first push

---
+++ src/main/java/com/approot/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.approot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("home")
+public class HomeController {
+	@RequestMapping(method = RequestMethod.GET)
+    public String home() {
+        return "{\"hello\": \"product World\"}";
+    }
+
+}
